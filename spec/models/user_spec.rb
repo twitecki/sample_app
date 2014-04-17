25485dc0932f908a0553842a03c37e0eb9c434df
twitecki <witeckitom@gmail.com>
2014-04-17 00:01:31 -0500

    Adding Sign-in-out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,19 @@ describe User do
 
 	subject { @user }
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 	it { should respond_to(:name) } #making sure the name field is in the database for User
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
+	
 
 	it { should be_valid }
 
